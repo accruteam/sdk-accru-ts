3d314fadd093f5ad02a57e26e15c94d2bf42b814
Maddy VandenHeuvel <maddy.vandenheuvel@gmail.com>
2024-01-26 16:39:58 -0800

    feat: feat: org and collaborator services

--- src/services/organizations/organizations.service.ts
+++ src/services/organizations/organizations.service.ts
@@ -6,17 +6,45 @@ import {
 import {
   UserOrganizationBaseSettingsQuery,
   UserOrganizationBaseSettingsQueryVariables,
+  UserOrganizationCollaboratorDeleteMutation,
+  UserOrganizationCollaboratorDeleteMutationVariables,
+  UserOrganizationCollaboratorsQuery,
+  UserOrganizationCollaboratorsQueryVariables,
+  UserOrganizationInviteCollaboratorCreateMutation,
+  UserOrganizationInviteCollaboratorCreateMutationVariables,
+  UserOrganizationLogoPictureRemoveMutation,
+  UserOrganizationLogoPictureRemoveMutationVariables,
+  UserOrganizationQuery,
+  UserOrganizationQueryVariables,
   UserOrganizationUpdateMutation,
   UserOrganizationUpdateMutationVariables,
 } from '@api/gql/graphql';
 import { Res } from '@utils/response.type';
 import {
+  DELETE_ORGANIZATION_COLLABORATOR_MUTATION,
+  DELETE_ORGANIZATION_LOGO_MUTATION,
   GET_ORGANIZATION_BASE_SETTINGS_QUERY,
+  GET_ORGANIZATION_COLLABORATORS_QUERY,
+  GET_ORGANIZATION_QUERY,
+  INVITE_ORGANIZATION_COLLABORATOR_MUTATION,
   UPDATE_ORGANIZATION_MUTATION,
 } from './organizations.queries';
 
 export default class Organizations {
   constructor(private readonly apolloClient: ApolloClient<unknown>) {}
+
+  public get = async (
+    variables: UserOrganizationQueryVariables,
+    apolloClientOptions?: QueryOptions,
+  ): Promise<Res<UserOrganizationQuery>> => {
+    const { data } = await this.apolloClient.query({
+      ...apolloClientOptions,
+      query: GET_ORGANIZATION_QUERY,
+      variables,
+    });
+
+    return data.userOrganization;
+  };
 
   public getSettings = async (
     variables: UserOrganizationBaseSettingsQueryVariables,
@@ -42,5 +70,49 @@ export default class Organizations {
     });
 
     return data.userOrganization;
+  };
+
+  public getCollaborators = async (
+    variables: UserOrganizationCollaboratorsQueryVariables,
+  ): Promise<Res<UserOrganizationCollaboratorsQuery>> => {
+    const { data } = await this.apolloClient.query({
+      query: GET_ORGANIZATION_COLLABORATORS_QUERY,
+      variables,
+    });
+
+    return data.userOrganizationCollaborators;
+  };
+
+  public inviteCollaborator = async (
+    variables: UserOrganizationInviteCollaboratorCreateMutationVariables,
+  ): Promise<Res<UserOrganizationInviteCollaboratorCreateMutation>> => {
+    const { data } = await this.apolloClient.query({
+      query: INVITE_ORGANIZATION_COLLABORATOR_MUTATION,
+      variables,
+    });
+
+    return data.userOrganizationInviteCollaboratorCreate;
+  };
+
+  public deleteCollaborator = async (
+    variables: UserOrganizationCollaboratorDeleteMutationVariables,
+  ): Promise<Res<UserOrganizationCollaboratorDeleteMutation>> => {
+    const { data } = await this.apolloClient.query({
+      query: DELETE_ORGANIZATION_COLLABORATOR_MUTATION,
+      variables,
+    });
+
+    return data.userOrganizationCollaboratorDelete;
+  };
+
+  public deleteOrganizationLogo = async (
+    variables: UserOrganizationLogoPictureRemoveMutationVariables,
+  ): Promise<Res<UserOrganizationLogoPictureRemoveMutation>> => {
+    const { data } = await this.apolloClient.query({
+      query: DELETE_ORGANIZATION_LOGO_MUTATION,
+      variables,
+    });
+
+    return data.userOrganizationLogoPictureRemove;
   };
 }
