3e552ee7e305704869c5b2e7042af597af7c1350
luizfelipelaviola <luiz@laviola.dev>
2024-08-02 16:46:02 -0300

    feat: add error handler callbacks
    re ACR-1306

--- src/api/apolloClient.ts
+++ src/api/apolloClient.ts
@@ -4,6 +4,8 @@ import {
   createHttpLink,
   ApolloLink,
 } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+import { GraphQLErrors, NetworkError } from '@apollo/client/errors';
 import { setContext } from '@apollo/client/link/context';
 import { withScalars } from 'apollo-link-scalars';
 import {
@@ -18,8 +20,11 @@ import introspectionResult from './gql/schema.graphql.json';
 interface IAccruClientParams {
   baseUrl: string;
 
-  token?: string;
   getAuthToken?: () => Promise<string>;
+
+  onGraphQLError?: (errors: GraphQLErrors) => void;
+  onNetworkError?: (error: NetworkError) => void;
+  onAuthError?: () => void;
 }
 
 // eslint-disable-next-line func-names
@@ -72,8 +77,27 @@ const schema = buildClientSchema(
 export const createApolloClient = ({
   baseUrl,
   getAuthToken,
-  token,
+
+  onGraphQLError,
+  onNetworkError,
+  onAuthError,
 }: IAccruClientParams) => {
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors?.length && typeof onGraphQLError === 'function')
+      onGraphQLError(graphQLErrors);
+
+    if (networkError && typeof onNetworkError === 'function')
+      onNetworkError(networkError);
+
+    if (
+      graphQLErrors?.some(
+        error => error.extensions?.code === 'UNAUTHENTICATED',
+      ) &&
+      typeof onAuthError === 'function'
+    )
+      onAuthError();
+  });
+
   const scalarLink = withScalars({
     schema,
     typesMap: {
@@ -81,13 +105,11 @@ export const createApolloClient = ({
     },
   });
 
-  const httpLink = createHttpLink({ uri: baseUrl });
-
   const authLink = setContext(async (_, { headers }) => {
     const selectedToken =
       typeof getAuthToken === 'function'
-        ? (await getAuthToken()) || token
-        : token;
+        ? (await getAuthToken()) || null
+        : null;
 
     return {
       headers: {
@@ -99,8 +121,10 @@ export const createApolloClient = ({
     };
   });
 
+  const httpLink = createHttpLink({ uri: baseUrl });
+
   return new ApolloClient({
-    link: ApolloLink.from([authLink, scalarLink, httpLink]),
+    link: ApolloLink.from([errorLink, scalarLink, authLink, httpLink]),
     cache: new InMemoryCache(),
   });
 };
